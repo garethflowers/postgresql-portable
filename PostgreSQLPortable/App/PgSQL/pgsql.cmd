e57b2ba1006b3744f2473a1497d1320e0fda4ca7
Gareth Flowers <garethflowers@users.noreply.github.com>
2015-07-05 00:37:58 +0100

    Moved environment variables to launcher.ini

--- PostgreSQLPortable/App/PgSQL/pgsql.cmd
+++ PostgreSQLPortable/App/PgSQL/pgsql.cmd
@@ -1,22 +1,9 @@
 @echo off
-title PostgreSQL 8.4
+title PostgreSQL Portable
 cls
 
 :: set default code page
 chcp 1252 > nul
-
-:: find base directories
-for %%? in ("%~dp0..\..") do set APPBASE=%%~f?
-for %%? in ("%~dp0..\..\..") do set BASE=%%~f?
-
-:: set up postgres variables
-set PGSQL=%APPBASE%\App\PgSQL
-set PGDATA=%APPBASE%\Data\data
-set PGLOG=%APPBASE%\Data\log.txt
-set PGLOCALEDIR=%PGSQL%\share\
-set PGDATABASE=postgres
-set PGUSER=postgres
-set PATH=%PGSQL%\bin;%PATH%
 
 :: initialise a new database on first use
 if not exist "%PGDATA%" (
